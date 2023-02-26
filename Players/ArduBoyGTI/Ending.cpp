8296e47ac830d9a0942ae84f04f02bdc772f2141
Pharap <2933055+Pharap@users.noreply.github.com>
2023-02-26 13:59:53 +0000

    Implement awaitKeyEnding
    Implements the custom `endinganykey` behaviour from "Shadows and Eternity" by @Dolorre.
    (See [Shadows and Eternity](https://github.com/Dolorre/ShadowsandEternity).)
    
    Co-authored-by: Dolorre <75703911+Dolorre@users.noreply.github.com>

--- Players/ArduBoyGTI/Ending.cpp
+++ Players/ArduBoyGTI/Ending.cpp
@@ -31,61 +31,65 @@ void drawEnding()
 }
 
 void awaitKeyEnding()
-{ 
-  arduboy.fillRect(arduboy.getCursorX(), arduboy.getCursorY(), 8, 8, BLACK);
-  updateDisplay(); 
+{
+	arduboy.fillRect(arduboy.getCursorX(), arduboy.getCursorY(), 8, 8, BLACK);
+	updateDisplay(); 
 
-  #if SOUND == ON
-    sound.tone(600, 50);
-    delay(50);
+	#if SOUND == ON
+		sound.tone(600, 50);
+		delay(50);
 
-    sound.tone(1318, 50);
-    delay(50);
+		sound.tone(1318, 50);
+		delay(50);
 
-    sound.tone(900, 50);
-    delay(50);
-  #endif
+		sound.tone(900, 50);
+		delay(50);
+	#endif
 
-  bool blink { false };
-  uint8_t counter { 0 };
+	bool blink { false };
+	uint8_t counter { 0 };
 
-  while(true)
-  {
-    if(arduboy.pressed(FIRE_BUTTON))
-      break;
+	while(true)
+	{
+		if(arduboy.pressed(FIRE_BUTTON))
+			break;
 
-    if(arduboy.pressed(JUMP_BUTTON))
-      break;
+		if(arduboy.pressed(JUMP_BUTTON))
+			break;
 
-    #if SERIAL == ON
-    if(Serial.available())
-    {
-      Serial.read();
-      Serial.read();
-      Serial.read();
-      break;
-    } 
-    #endif
+		#if SERIAL == ON
+		if(Serial.available())
+		{
+			Serial.read();
+			Serial.read();
+			Serial.read();
+			break;
+		} 
+		#endif
 
-    ++counter;
+		++counter;
 
-    if(counter > 40)
-    {
-      counter = 0;
-      blink = !blink;
+		if(counter > 40)
+		{
+			counter = 0;
+			blink = !blink;
 
-      // Draw blinking cursor
-      arduboy.fillRect(120, 55, 10, 10, blink ? WHITE : BLACK);
-      updateDisplay();
-    }
+			// Draw blinking cursor
+			if(blink)
+				arduboy.fillRect(125, 60, 1, 1, WHITE);
+			else
+				arduboy.fillRect(120, 55, 10, 10, BLACK);
+		 
+			updateDisplay();
+		}
 
-    delay(10);
-  }
+		delay(10);
+	}
 
-  arduboy.clear();
-  updateDisplay();
+	arduboy.clear();
+	updateDisplay();
 
-  #if SERIAL == ON
-  Serial.println("\n"); 
-  #endif
+	#if SERIAL == ON
+	Serial.println("\n"); 
+	#endif
 }
