9dfbcd7c02c849fa78894e724443253e1f9d6c5e
Pharap <2933055+Pharap@users.noreply.github.com>
2023-02-26 06:23:49 +0000

    Move code reading functions into CodeReading.h

--- Players/ArduBoyGTI/ArduBoyGTI.ino
+++ Players/ArduBoyGTI/ArduBoyGTI.ino
@@ -14,6 +14,7 @@ ArduboyTones sound(arduboy.audio.enabled);
 
 #include "Settings.h"
 #include "Bytecode.h"
+#include "CodeReading.h"
 
 // Init system variables
 
@@ -32,58 +33,6 @@ PROGMEM const unsigned char arduino [] = {
 
 char exita[80] {};
 char exitb[80] {};
-
-uint16_t pc { 0 };
-
-uint8_t readByte()
-{
-  // Read the byte
-  const uint8_t result { pgm_read_byte(&gti[pc]) };
-
-  // Increment the program counter
-  ++pc;
-
-  // Return the result
-  return result;
-}
-
-uint16_t readWord()
-{
-  // Read the high byte
-  const uint8_t high { readByte() };
-
-  // Read the low byte
-  const uint8_t low  { readByte() };
-
-  // Combine the bytes into a single result
-  const uint16_t result { (static_cast<uint16_t>(high) << 8) | (static_cast<uint16_t>(low) << 0) };
-
-  // Return the result
-  return result;
-}
-
-char readCharacter()
-{
-  return readByte();
-}
-
-template<size_t size> void readString(char (&buffer)[size])
-{
-  // Avoid a buffer overrun
-  for(uint8_t index = 0; index < size; ++index)
-  {
-    // Read a character from the input
-    const char character { readCharacter() };
-
-    // Copy the character to the buffer
-    buffer[index] = character;
-
-    // If the character is null
-    if(character == '\0')
-      // Exit
-      return;
-  }
-}
 
 template<size_t size> void printString(char (&buffer)[size])
 {
